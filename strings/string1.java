5e70309cb4e8952fe1a0c065f487259d5a3407bc
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-06 20:58:42 +0530

    programms

--- strings/string1.java
+++ strings/string1.java
@@ -25,14 +25,25 @@ public class string1 {
          * 1.--> name.length (RETURN THE LENGTH OFF THE STRING NAME)
          * 2.--> name.toLowerCase (THIS WILL CONVERT ALL THE UPPER CASE ELEMENTS INTO
          * LOWER CASE)
-         * 3.-->
+         * 3.--> NonTrimmedString
+         * 4.--> trim
+         * 5.-->
          */
         System.out.println("Displaying the length of your string : ");
         System.out.println(str.length());
+        String lstr = str;
         System.out.println("Converting the upper case elements into lower case elements : ");
-        System.out.print(str.toLowerCase());
+        System.out.print(lstr.toLowerCase());
+        String ustr = str;
         System.out.println("\nConverting the lower case elements into lower case elements : ");
-        System.out.print(str.toUpperCase());
+        System.out.print(ustr.toUpperCase());
+
+        System.out.println("\nNon trimmed string ");
+        String nonTrimmedString = "             Ankush saini";
+        System.out.println(nonTrimmedString);
+        System.out.println("Differnce using the trim in nontrimmedString");
+        String trim = nonTrimmedString.trim();
+        System.out.println(trim);
         in.close();
     }
 }
