82a3fa90f210ae96b414bd6112ce5727da1d3a40
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-08-25 11:41:57 +0530

    programm

---
+++ Mathematic/ques1.java
@@ -0,0 +1,10 @@
+package Mathematic;
+
+/**
+ * ques1
+ */
+public class ques1 {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ OOPS/ques1.java
@@ -0,0 +1,12 @@
+package OOPS;
+class Student
+{
+    int id=12;
+    String name="Ankush saini";
+    public static void main(String[] args) {
+        System.out.println("OOPs First Programm in java ");
+        Student st=new Student();
+        System.out.println("Student Id : "+st.id);
+        System.out.println("Student name : "+st.name);
+    }
+}
