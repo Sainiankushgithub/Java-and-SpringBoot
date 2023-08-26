ff3a73d8ebe372fb7f151eb4906651eda77b173e
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-08-26 22:15:07 +0530

    programm

---
+++ Java Lab/ques4.java
@@ -0,0 +1,44 @@
+package Class;
+
+import java.util.Scanner;
+
+class Palindrome
+{
+    int num;
+    Palindrome(int num)
+    {
+        this.num=num;
+    }
+    boolean check(int num)
+    {
+        int temp=num;
+        int sum=0;
+        while(num!=0)
+        {
+            int r=num%10;
+            sum=sum*10+r;
+            num=num/10;
+        }
+        if(sum==temp)
+        {
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int num;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your number to check whether entered number number is palindrome ");
+        num=sc.nextInt();
+        Palindrome p= new Palindrome(num);
+        if(p.check(num)==true)
+        {
+            System.out.println("Entered number "+num+" is palindrome");
+        }
+        else
+        {
+            System.out.println("Entered number "+num+" is not palindrome");
+        }
+        sc.close();
+    }
+}
