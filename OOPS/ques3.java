a1903a8f2d482c2054cf3d989abe7a408b616078
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-08-30 22:53:22 +0530

    programm

---
+++ OOPS/ques3.java
@@ -0,0 +1,45 @@
+package OOPS;
+import java.util.*;
+class Construct {
+
+    String Name;
+    int roll_no;
+    String Section;
+    int age;
+
+    // CONSTRUCTOR 
+
+    Construct(String Name,int roll_no,String Section,int age)
+    {
+        this.Name=Name;
+        this.roll_no=roll_no;
+        this.Section=Section;
+        this.age=age;
+    }
+    void display()
+    {
+        System.out.println("Name : "+Name);
+        System.out.println("Roll number : "+roll_no);
+        System.out.println("Section : "+Section);
+        System.out.println("Age : "+age);
+    }
+    public static void main(String[] args) {
+        String name;
+        int roll_no;
+        String Section;
+        int age;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        name=sc.nextLine();
+        System.out.println("Enter your roll number : ");
+        roll_no=sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter your Section : ");
+        Section=sc.nextLine();
+        System.out.println("Enter your age : ");
+        age=sc.nextInt();
+        Construct cn=new Construct(name,roll_no,Section,age);
+        cn.display();
+        sc.close();
+    }
+}
