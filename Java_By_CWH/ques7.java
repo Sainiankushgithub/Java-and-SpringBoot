6eec9eb10e70c31f1c1f69ca7ee61a9576e1d3a4
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-09-05 16:40:08 +0530

    programm

--- Java_By_CWH/ques7.java
+++ Java_By_CWH/ques7.java
@@ -5,10 +5,39 @@ class Employee
     private String Name;
     private int ID;
     private int Salary;
-    private String desig
+    private String Designation;
+    Employee(String name,int id ,int salary,String designation)
+    {
+        this.Name=name;
+        this.ID=id;
+        this.Salary=salary;
+        this.Designation=designation;
+    }
+    public void display()
+    {
+        System.out.println("Name : "+Name);
+        System.out.println("ID : "+ID);
+        System.out.println("Salary : "+Salary);
+        System.out.println("Designation : "+Designation);
+    }
 }
 public class ques7 {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter all the details of the Empoyee ");
+        String name1,designation1;
+        int id1,salary1;
+        System.out.println("Enter the name of the Empoyee ");
+        name1=sc.nextLine();
+        System.out.println("Enter the ID number of the Empoyee ");
+        id1=sc.nextInt();
+        System.out.println("Enter the salary of the Empoyee ");
+        salary1=sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter the Designation of the Empoyee ");
+        designation1=sc.nextLine();
+        Employee e=new Employee(name1, id1, salary1, designation1);
+        e.display();
+        sc.close();
     }
 }
