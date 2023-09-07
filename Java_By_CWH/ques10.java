0929912075710805216c4bdbcec54c7aa722e97c
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-09-07 22:34:38 +0530

    programming

---
+++ Java_By_CWH/ques10.java
@@ -0,0 +1,51 @@
+package Java_By_CWH;
+import java.util.*;
+class Base1
+{
+    Base1()
+    {
+        System.out.println("I'm Base class constructor ");
+    }
+    Base1(int x)
+    {
+        System.out.println("I'm constructor with  x value as : "+x);
+    }
+}
+class Derived1 extends Base1
+{
+    Derived1()
+    {
+        // super(12);
+        System.out.println("I'm Derived class constructor");
+    }
+    Derived1(int x,int y)
+    {
+        super(x);
+        System.out.println("I'm overloaded Constructor with value of y as : "+y);
+    }
+}
+class childDerived1 extends Derived1
+{
+    childDerived1()
+    {
+        System.out.println("I'm childderived constructor ");
+    }
+    childDerived1(int x,int y,int z)
+    {
+        super(x,y);
+        System.out.println("I'm childDerived overloaded constructor with the value of z as : "+z);
+    }
+}
+public class ques10 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        // Base1 b=new Base1();
+        // Derived1 d=new Derived1();
+        // Derived1 d=new Derived1(14, 23);
+
+        // childDerived1 cd=new childDerived1();
+
+        childDerived1 cd=new childDerived1(12, 23, 34);
+        sc.close();
+    }
+}
