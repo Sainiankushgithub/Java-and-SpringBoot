d7cc4fe7efff333835930b951a638e287ca85e9d
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-09-08 16:17:03 +0530

    programming

---
+++ Java_By_CWH/ques11.java
@@ -0,0 +1,12 @@
+package Java_By_CWH;
+import java.util.*;
+class Inheritance
+{
+
+}
+public class ques11 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        sc.close();
+    }
+}
