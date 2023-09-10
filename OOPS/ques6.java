dfb6595f9ea39b81f1867e5fbc13b10a22ae4ae8
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-09-10 12:22:45 +0530

    programm

--- OOPS/ques6.java
+++ OOPS/ques6.java
@@ -1,8 +1,6 @@
 package OOPS;
 class Cylinder
 {
-    // GETTER AND SETTER 
-
     private int radius;
     private int height;
     public void setradius(int radius)
@@ -21,13 +19,33 @@ class Cylinder
     {
         return height;
     }
+
+    public double volume()
+    {
+        return Math.PI*radius*radius*height;
+    }
+    public double surfaceArea()
+    {
+        return 2*Math.PI*radius*radius+2*Math.PI*radius*height;
+    }
 }
 public class ques6 {
     public static void main(String[] args) {
+
+
+        //  PROBLEM NUMBER 1 
+
+
         Cylinder c=new Cylinder();
-        c.setradius(12);
+        c.setradius(9);
         System.out.println("Radius : "+c.getradius());
-        c.setheight(34);
+        c.setheight(12);
         System.out.println("Height : "+c.getheight());
+
+        // PROBLEM NUMBER 2  
+
+
+        System.out.println("Surface Area of a cylinder : "+c.surfaceArea());
+        System.out.println("Volume of a Cylinder : "+c.volume());
     }
 }
