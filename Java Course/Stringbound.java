030d91676c1ec44b69165c468f969618580b7cc2
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-11-24 15:23:57 +0530

    programm

--- Java Course/Stringbound.java
+++ Java Course/Stringbound.java
@@ -3,18 +3,32 @@ import java.util.*;
 public class Stringbound {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        System.out.println("Enter your name ");
-        String x=sc.nextLine();
+        // char []ch={'A','N','K','U','S','H'};
+        // try
+        // {
+        //     System.out.println(ch[5]);
+        // }
+        // catch(StringIndexOutOfBoundsException Se)
+        // {
+        //    System.out.println("Exception caught ");
+        //    Se.printStackTrace();
+        // }
+        System.out.println("Enter your String ");
+        String str=sc.nextLine();
         try
         {
-            for(int i=0;i<x.length()-1;i++)
+            System.out.println("Displaying your String ");
+            for(int i=0;i<=str.length();i++)
             {
-                System.out.println(x[i]);
+                System.out.println(str.charAt(i));
             }
         }
         catch(StringIndexOutOfBoundsException Se)
         {
-           System.out.println("Exception caught ");
+            Se.printStackTrace();
         }
+        System.out.println("Successfully executed ");
+        sc.close();
+        
     }
 }
---
+++ Java Course/Throws1.java
@@ -0,0 +1,7 @@
+package Java_Course;
+
+public class Throws1 {
+    public static void main(String[] args) {
+        
+    }
+}
