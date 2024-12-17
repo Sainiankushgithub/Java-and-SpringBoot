6d28502c63ad33d258dc9b173f2c27f3026f3070
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2024-12-17 22:41:30 +0530

    programm

--- Spring/Eclipse_Project/namedParametersSpringJdbc/src/main/java/in/sp/main/App.java
+++ Spring/Eclipse_Project/namedParametersSpringJdbc/src/main/java/in/sp/main/App.java
@@ -1,9 +1,36 @@
 package in.sp.main;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import in.sp.resources.springConfig;
+
 public class App 
 {
     public static void main( String[] args )
     {
+    	ApplicationContext context  = new AnnotationConfigApplicationContext(springConfig.class);
     	
+    	NamedParameterJdbcTemplate npjdbcTemplate=context.getBean(NamedParameterJdbcTemplate.class);
+    	
+    	Map<String, Object>map = new HashMap<String,Object>();
+    	map.put("key_roll",2206419);
+    	map.put("key_name", "Suhani sharma");
+    	map.put("key_marks",494);
+    	
+    	String insert_sql_query="INSERT INTO Student VALUES (:key_name,:key_roll,:key_marks)";
+    	
+    	int count=npjdbcTemplate.update(insert_sql_query, map);
+    	if(count>0) {
+    		System.out.println("Insetion successfull");
+    	}
+    	else {
+    		System.out.println("Insetion failed");
+    	}
+    			
     }
 }
--- Spring/Eclipse_Project/namedParametersSpringJdbc/src/main/java/in/sp/resources/springConfig.java
+++ Spring/Eclipse_Project/namedParametersSpringJdbc/src/main/java/in/sp/resources/springConfig.java
@@ -1,21 +1,24 @@
 package in.sp.resources;
-
-import java.sql.DriverManager;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 @Configuration
 public class springConfig {
 	@Bean
 	public DriverManagerDataSource myDataSource() {
-		DriverManagerDataSource dataSource= new DriverManagerDataSource();
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/spring_jdbc_db"); // localhost with data base name
 		dataSource.setUsername("root");
 		dataSource.setPassword("Ankush");
 		return dataSource;
 	}
+
 	@Bean
-	public 
+	public NamedParameterJdbcTemplate myJdbcTemplate() {
+		NamedParameterJdbcTemplate jdbctemplate = new NamedParameterJdbcTemplate(myDataSource());
+		return jdbctemplate;
+	}
 }
